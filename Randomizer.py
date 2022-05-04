97e1919551144ade3a8aed4f14d4fa7d47435308
Noël <noel_khalaf@hotmail.com>
2022-05-04 00:22:10 -0400

    Added Entries functionality, regex, and gifs

--- Randomizer.py
+++ Randomizer.py
@@ -1,3 +1,5 @@
+import os
+import time
 import random
 import discord
 from discord.ext import commands
@@ -7,31 +9,57 @@ class Randomizer:
     cards = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
     dieValues = ('1', '2', '3', '4', '5', '6')
     coinValues = ('Heads', 'Tails')
-
+    
     async def coin(self, ctx):
         """
         flips a coin
         """
-        await ctx.send("```ini\nYou flipped {}!\n```".format(self.getRandomCoin(ctx)))
+        result = self.getRandomCoin(ctx)
+        if result == 'Heads':
+            await ctx.send(file=discord.File('images/HeadsFlip.gif'))
+        else:
+            await ctx.send(file=discord.File('images/TailsFlip.gif'))
+        time.sleep(6)
+        await ctx.send("```ini\nYou flipped [{}]!\n```".format(result))
 
     async def coinChoices(self, ctx, choices):
         """
-        flips a coin.
+        flips a coin with choices
         :param args: coin flip choices
-        """        
-        await ctx.send("```ini\nYou flipped {}!\n```".format(self.getRandomCoinChoices(ctx, choices[0], choices[1])))
+        """
+        result = self.getRandomChoice(ctx, choices)
+        if result == choices[0]:
+            await ctx.send(file=discord.File('images/HeadsFlip.gif'))
+        else:
+            await ctx.send(file=discord.File('images/TailsFlip.gif'))
+        time.sleep(6)
+        await ctx.send("```ini\nYou flipped [{}]!\n```".format(result))
 
     async def die(self, ctx):
         """
         rolls a die
         """
-        await ctx.send("```ini\nYou rolled a {}!\n```".format(self.getRandomDie(ctx)))
+        result = self.getRandomDie(ctx)
+        if result == '1':
+            await ctx.send(file=discord.File('images/dice-1.gif'))
+        elif result == '2':
+            await ctx.send(file=discord.File('images/dice-2.gif'))
+        elif result == '3':
+            await ctx.send(file=discord.File('images/dice-3.gif'))
+        elif result == '4':
+            await ctx.send(file=discord.File('images/dice-4.gif'))
+        elif result == '5':
+            await ctx.send(file=discord.File('images/dice-5.gif'))
+        elif result == '6':
+            await ctx.send(file=discord.File('images/dice-6.gif'))
+        time.sleep(8)
+        await ctx.send("```ini\nYou rolled a [{}]!\n```".format(result))
 
     async def card(self, ctx):
         """
         draws a card
         """
-        await ctx.send("```ini\nYou drew a {}!\n```".format(self.getRandomCard(ctx)))
+        await ctx.send("```ini\nYou drew a [{}]!\n```".format(self.getRandomCard(ctx)))
 
     async def range(self, ctx, low, high):
         """
@@ -39,20 +67,111 @@ class Randomizer:
         :param low: low number
         :param high: high number
         """
-        await ctx.send("```ini\nYou got {}!\n```".format(self.getRandomRange(ctx, low, high)))
+        await ctx.send("```ini\nYou got [{}]!\n```".format(self.getRandomRange(ctx, low, high)))
 
     async def list(self, ctx, args):
         """
         randomizes from listed options in args
         :param args: possible randomization outcomes
         """
-        await ctx.send("```ini\nYou got {}!\n```".format(self.getRandomList(ctx, args.split())))
+        await ctx.send("```ini\nYou got [{}]!\n```".format(self.getRandomChoice(ctx, args)))
+
+    async def entries(self, ctx, num):
+        """
+        randomizes from entries in entries.txt
+        """
+        with open("textfiles/entries.txt", "r") as f:
+            entries = f.readlines()
+        entries = [entry for entry in entries if entry != "\n"]
+        if await self.isFileEmpty(ctx, entries): return
+        entries_names = [entry[:-1] if entry[-1:] == "\n" else entry for entry in entries]
+        for i in range(num):
+            await ctx.send("```ini\nYou got [{}]!\n```".format(self.getRandomChoice(ctx, entries_names)))
+
+    async def getfile(self, ctx):
+        """
+        returns the entries.txt file to view entries
+        """ 
+        await ctx.send("```ini\nHere is the text file with the entries:\n```")
+        await ctx.send(file=discord.File('textfiles/entries.txt'))
+
+    #async def setfile(self, ctx):
+        """
+        replaces entries in entries.txt file with new file
+        """ 
+        #await ctx.send("```ini\nHere is the text file with the entries:\n```")
+        #await ctx.send(file=discord.File('entries.txt'))
+
+    async def sortfile(self, ctx):
+        """
+        sorts the entries in the entries.txt file alphabetically
+        """
+        with open("textfiles/entries.txt", "r+") as f:
+            entries = f.readlines()
+        newentries = [entry+"\n" if entry[-1] !="\n" else entry for entry in entries if entry != "\n"]
+        if await self.isFileEmpty(ctx, newentries): return
+        newentries.sort()
+        self.replaceEntries(ctx, newentries)
+        await ctx.send("```ini\nEntries in Entries sorted successfully!\n```")
+
+    async def cleanfile(self, ctx):
+        """
+        filters out duplicate entries from the entries.txt file
+        """
+        with open("textfiles/entries.txt", "r+") as f:
+            entries = f.readlines()
+        entries = [entry for entry in entries if entry != "\n"]
+        if await self.isFileEmpty(ctx, entries): return
+        newentries = list(dict.fromkeys(entries))
+        self.replaceEntries(ctx, newentries)
+        numdups = len(entries) - len(newentries)
+        await ctx.send("```ini\n[{}] duplicates in Entries removed successfully!\n```".format(numdups))
+
+    async def addentries(self, ctx, args):
+        """
+        appends specified entries to the entries.txt file
+        :param args: list of entries
+        """
+        with open("textfiles/entries.txt", "r+") as f:
+            entries = f.readlines()
+            entries = [entry.lower() for entry in entries]
+            if entries[-1][-1:] != "\n":
+                f.write("\n")
+            for arg in args:
+                if arg.lower() in entries:
+                    await ctx.send("```ini\nEntry [{}] is already in the list of Entries\n```".format(arg))
+                else:
+                    f.write(arg+"\n")
+                    await ctx.send("```ini\n[{}] successfully added to Entries!\n```".format(arg))
+
+    async def removeentries(self, ctx, args):
+        """
+        removes specified entries from the entries.txt file
+        :param args: list of entries
+        """
+        with open("textfiles/entries.txt", "r+") as f:
+            entries = f.readlines()
+            if await self.isFileEmpty(ctx, entries): return
+            newentries = entries
+            entries = [entry[:-1].lower() for entry in entries]
+            for arg in args:
+                if arg not in entries:
+                    await ctx.send("```ini\nEntry [{}] is not in the list of Entries\n```".format(arg))
+                    continue
+                count = 0
+                for i, entry in enumerate(entries):
+                    if entry == arg.lower():
+                        newentries[i] = ""
+                        count += 1
+                    if i == len(entries)-1:
+                        await ctx.send("```ini\nSuccessfully removed {} instance(s) of [{}] from Entries!\n```".format(count,arg))
+        self.replaceEntries(ctx, newentries)
 
     def getRandomCoin(self, ctx):
         return random.choice(self.coinValues)
 
-    def getRandomCoinChoices(self, ctx, arg1, arg2):
-        return random.choice([arg1, arg2])
+    def getRandomChoice(self, ctx, choices):
+        return random.choice(choices)
 
     def getRandomDie(self, ctx):
         return random.choice(self.dieValues)
@@ -63,5 +182,19 @@ class Randomizer:
     def getRandomRange(self, ctx, arg1, arg2):
         return random.randrange(arg1, arg2)
 
-    def getRandomList(self, ctx, args):
-        return random.choice(args)
+    async def isFileEmpty(self, ctx, entries):
+        if not entries:
+            await ctx.send("```ini\nThere are no entries to choose from. Use '.addentry <entry1>/<entry2>/...' or '.setfile <file.txt>' to add entries!\n```")
+            return True
+        return False
+
+    def replaceEntries(self, ctx, newentries):
+        with open("textfiles/entries.txt", "r+") as f:
+            f.seek(0)
+            for entry in newentries:
+                if entry[-1:] != "\n":
+                    f.write(entry+"\n")
+                else:
+                    f.write(entry)
+            f.truncate()
+    
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 import discord
 from discord.ext import commands
 from Randomizer import Randomizer
+import re
 from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask
 from threading import Thread
-
+""""
 app=Flask("")
 
 @app.route("/")
@@ -14,13 +15,21 @@ def index():
     return "<h1>Bot is running</h1>"
 
 Thread(target=app.run,args=("0.0.0.0",8080)).start()
-
+"""
 aliases_dict = {
     'coin' : ['flip','toss','flick'],
     'die' : ['dice','roll'],
     'card' : ['deck','draw'],
     'range' : ['num','number','from'],
     'list' : ['options'],
+    'entries' : ['entry','prompt'],
+    'getfile' : ['file'],
+    'setfile' : ['newfile', 'changefile'],
+    'sortfile' : ['alpha', 'alphabetical', 'sort', 'arrange', 'orderfile', 'order'],
+    'cleanfile' : ['clean', 'filterfile', 'filter', 'duplicates', 'dup', 'dups', 'removeduplicates'],
+    'addentries' : ['add', 'addentry', 'newentries'],
+    'removeentries' : ['remove', 'removeentry'],
+    'aliases' : ['alias'],
 }
 
 client = commands.Bot(command_prefix = '.')
@@ -40,22 +49,30 @@ async def on_command_error(ctx, error):
 async def commands(ctx):
   await ctx.send('```bash\n\
 The Score Settler commands:\n\
-".coin" = Flips a two-sided coin.\n\
-".coin <this> <that>" = Flips with what is at stake.\n\
-".die" = Rolls a six-sided die.\n\
-".card" = Draws a card from a 52-card deck.\n\
-".range <low> <high>" = Randomizes an integer between numbers low and high.\n\
-".list <thing1> <thing2> <thing3> ..." = Chooses at random from a list of options.\n\
-".aliases <command>" = Shows the different aliases relating to specific commands.\
-                ```')
+\'.coin\' = Flips a two-sided coin.\n\
+\'.coin \"<this>\" \"<that>\"\' = Flips with what is at stake.\n\
+\'.die\' = Rolls a six-sided die.\n\
+\'.card\' = Draws a card from a 52-card deck.\n\
+\'.range <low> <high>\' = Randomizes a number between numbers low and high.\n\
+\'.list \"<thing1>\" \"<thing2>\" ...\' = Chooses at random from a list of options.\n\
+\'.entries\' = Returns a random entry from the list of Entries.\n\
+\'.entries <number>\' = Returns a specific number of random entries from the list of Entries.\n\
+\'.getfile\' = Returns the list of Entries as a text file attachment.\n\
+\'.setfile <file.txt>\' = Sets the new list of Entries from the text file attachment.\n\
+\'.sortfile\' = Sorts the list of Entries alphabetically.\n\
+\'.cleanfile\' = Removes duplicates and empty lines from the list of Entries.\n\
+\'.addentries \"<thing1>\" \"<thing2>\" ...\' = Adds entries to the list of Entries.\n\
+\'.removeentries \"<thing1>\" \"<thing2>\" ...\' = Removes entries from the list of Entries.\n\
+\'.aliases <command>\' = Shows the different aliases relating to specific commands.\
+```')
 
 @client.command(aliases=aliases_dict['coin'])
 async def coin(ctx, *, args):
-    choices = args.split()
+    choices = [a if b == '' else b for (a,b) in re.findall("\"([\w\s]+)\"|(\w+)", args)]
     if len(choices) == 2:
         await randomizer.coinChoices(ctx, choices)
     else:
-        await ctx.send("```ini\nYou can either do '.flip' or '.flip <this> <that>'.\n```")
+        await ctx.send("```bash\nYou can either do '.flip' or '.flip \"<this>\" \"<that>\"'.\n```")
 @coin.error
 async def coin_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
@@ -72,15 +89,15 @@ async def card(ctx):
 @client.command(aliases=aliases_dict['range'])
 async def range(ctx, *args):
     if len(args) != 2:
-        await ctx.send("```ini\nType two integers to define the range of integers (inclusive) to randomize a number from '.range <low> <high>'.\n```")
+        await ctx.send("```bash\nType two integers to define the range of integers (inclusive) to randomize a number from '.range <low> <high>'.\n```")
         return
     try:
         range = [int(float(i)) for i in args]
     except ValueError:
-        await ctx.send("```ini\nType the range using only integers (inclusive) to randomize a number from '.range <low> <high>'.\n```")
+        await ctx.send("```bash\nType the range using only integers (inclusive) to randomize a number from '.range <low> <high>'.\n```")
         return
     if range[0] == range[1]:
-        await ctx.send("```ini\nYou got {}!\n```".format(range[0]))       
+        await ctx.send("```ini\nYou got [{}]!\n```".format(range[0]))       
     elif range[0] > range[1]:
         await randomizer.range(ctx, range[1], range[0])
     else:
@@ -88,31 +105,87 @@ async def range(ctx, *args):
 @range.error
 async def range_error(ctx, error):
 	if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-		await ctx.send("```ini\nType the range of integers (inclusive) to randomize a number from '.range <low> <high>'.\n```")
+		await ctx.send("```bash\nType the range of integers (inclusive) to randomize a number from '.range <low> <high>'.\n```")
 
 @client.command(aliases=aliases_dict['list'])
 async def list(ctx, *, args):
-    await randomizer.list(ctx, args)
+    choices = [a if b == '' else b for (a,b) in re.findall("\"([\w\s]+)\"|(\w+)", args)]
+    await randomizer.list(ctx, choices)
 @list.error
 async def list_error(ctx, error):
 	if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-		await ctx.send("```ini\nType a list of possible randomization options to choose from '.list <thing1> <thing2> <thing3> ...'.\n```")
+		await ctx.send("```bash\nType a list of possible randomization options to choose from '.list \"<thing1>\" \"<thing2>\" ...'.\n```")
 
+@client.command(aliases=aliases_dict['entries'])
+async def entries(ctx, arg):
+    try:
+        num = int(float(arg))
+    except ValueError:
+        await ctx.send("```bash\nType the number of entries to randomize using only integers '.entries <number>'.\n```")
+        return
+    if num>10 or num<1:
+        await ctx.send("```bash\nEnter a number of entries to randomize between 1 and 10 '.entries <number>'.\n```")
+        return
+    await randomizer.entries(ctx, num)
+@entries.error
+async def entries_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+        await randomizer.entries(ctx, 1)
 
-@client.command()
+@client.command(aliases=aliases_dict['getfile'])
+async def getfile(ctx):
+    await randomizer.getfile(ctx)
+"""
+@client.command(aliases=aliases_dict['setfile'])
+async def setfile(ctx, arg):
+    await randomizer.setfile(ctx, arg)
+"""
+@client.command(aliases=aliases_dict['sortfile'])
+async def sortfile(ctx):
+    await randomizer.sortfile(ctx)
+
+@client.command(aliases=aliases_dict['cleanfile'])
+async def cleanfile(ctx):
+    await randomizer.cleanfile(ctx)
+
+@client.command(aliases=aliases_dict['addentries'])
+async def addentries(ctx, *, args):
+    choices = [a if b == '' else b for (a,b) in re.findall("\"([\w\s]+)\"|(\w+)", args)]
+    await randomizer.addentries(ctx, choices)
+@addentries.error
+async def addentry_error(ctx, error):
+	if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+		await ctx.send("```bash\nType the entries you would like to add to the list of Entries '.addentries \"<entry1>\" \"<entry2>\" ...'.\n```")
+
+@client.command(aliases=aliases_dict['removeentries'])
+async def removeentries(ctx, *, args):
+    choices = [a if b == '' else b for (a,b) in re.findall("\"([\w\s]+)\"|(\w+)", args)]
+    await randomizer.removeentries(ctx, choices)
+@removeentries.error
+async def removeentry_error(ctx, error):
+	if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+		await ctx.send("```bash\nType the entries you would like to remove from the list of Entries'.removeentries \"<entry1>\" \"<entry2>\" ...'.\n```")
+
+@client.command(aliases=aliases_dict['aliases'])
 async def aliases(ctx, command):
     """
     shows all the aliases of a given command
     """
     if command in aliases_dict:
-        await ctx.send("```ini\nAliases for {} : {}.\n```".format(command, aliases_dict[command]))
-    else:
-        await ctx.send("```ini\n{} is not a command.\n```".format(command))
-        await commands(ctx)
+        await ctx.send("```ini\nAliases for '{}' : {}.\n```".format(command, aliases_dict[command]))
+        return
+
+    for key, value in aliases_dict.items():
+        if command in value:
+            aliases = [alias if alias != command else key for alias in value]
+            await ctx.send("```ini\nAliases for '{}' : {}.\n```".format(command, aliases))
+            return
+    await ctx.send("```bash\n'{}' is not on of the avaiable commands.\nYou can use \'.command\' to view all the commands.```".format(command))
+    #await commands(ctx)
 @aliases.error
 async def aliases_error(ctx, error):
 	if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-		await ctx.send("```ini\nSpecify a command to view its aliases '.aliases <command>'.\n```")
+		await ctx.send("```bash\nEnter one of the possible commands to view its aliases '.aliases <command>'.\n```")
 
 client.run(os.environ['TOKEN'])
 
